6f778cac59d1bf848f76a4ffad4d936a950ee246
David Kang <tk.kang@outlook.com>
2024-08-21 15:46:44 -0700

    changed astro.config.mjs to deploy to tkang7.github.io

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'tkang7.github.io',
+  // base: 'my-repo',
   integrations: [mdx(), sitemap(), tailwind()]
 });

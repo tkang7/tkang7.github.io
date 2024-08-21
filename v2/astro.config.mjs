f4042995788d37e885fd303951c1678899d9928d
David Kang <tk.kang@outlook.com>
2024-08-21 15:52:36 -0700

    v2

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'tkang7.github.io',
-  // base: 'my-repo',
+  base: '/v2',
   integrations: [mdx(), sitemap(), tailwind()]
 });

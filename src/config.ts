35fc389ce1c91ab921875930e997261b7dd85b65
David Kang <tk.kang@outlook.com>
2024-08-27 14:42:40 -0700

    adding SITE_DESCRIPTION to config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Taekyu (David) Kang';
+export const SITE_DESCRIPTION = "Personal Website"
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
